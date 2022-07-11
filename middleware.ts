a8e5c7328fba2fcb3bfff97b3e9bb02c99390821
Kirim <StatusQuo1337@gmail.com>
2022-07-11 13:10:54 +0500

    config rewrite changed

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from 'next/server'
 
 export async function middleware(request: NextRequest) {
   if (!request.cookies.has('qid')) {
-    return NextResponse.rewrite(new URL('/welcome', request.url))
+    return NextResponse.redirect(new URL('/welcome', request.url), 307)
   }
   return NextResponse.next()
 }
